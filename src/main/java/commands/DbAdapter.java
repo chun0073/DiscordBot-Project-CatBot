57eda8337901d75d1b2bd705908c64dd09c3b96e
PiggyCat <70961051+chun0073@users.noreply.github.com>
2021-01-19 14:04:24 -0500

    Update DbAdapter.java

--- src/main/java/commands/DbAdapter.java
+++ src/main/java/commands/DbAdapter.java
@@ -3,5 +3,5 @@ package commands;
 public interface DbAdapter {
     static String url = "jdbc:mysql://localhost:3306";
     static String username = "root";
-    static String password = "cjs1214413";
+    static String password = "3";
 }
